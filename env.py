8b5a348c36be31cd09efde074a14c4c96f05a37c
Alan O Shea <alanosheaclash@gmail.com>
2020-03-11 09:02:51 +0000

    Added environ files to env.py dev and app now running separately

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["SECRET_KEY"] = 'y5%8(7s&&mootj&$b9nr0kdk51fsjn$@edkxof(vka)d@$fnzk'
+os.environ["DEVELOPMENT"] = "1"
+os.environ["DATABASE_URL"] = 'postgres://jbpniwiuwxsokx:7c9c12208cbca86c2f7ca640df9d84a1661fb1617150fcfc0ea33ffacad2d09d@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/dpjto0gqjk5at'
+os.environ["HOSTNAME"] = 'localhost'
--- todo/env.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = 'y5%8(7s&&mootj&$b9nr0kdk51fsjn$@edkxof(vka)d@$fnzk'
